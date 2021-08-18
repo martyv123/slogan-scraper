5111fa2c388dc7a5bdeaf0748e01d1c62af70934
Marty <martyv123@ccs.neu.edu>
2021-08-17 21:45:20 -0400

    scraping complete

--- scraper-2.py
+++ scraper-2.py
@@ -6,14 +6,14 @@ from bs4 import BeautifulSoup
 SLOGANS = []
 
 # Getting the slogan categories page
-categories_page = requests.get("https://www.thinkslogans.com/submit-slogans/")
+categories_page = requests.get("https://www.thinkslogans.com/")
 categories_content = BeautifulSoup(categories_page.content, 'html.parser')
 
 # Finding all the category links and removing irrelevant ones
 category_links = []
 all_links = categories_content.find_all('a')
 for link in all_links:
-    if 'https://www.thinkslogans.com/slogans/' in link['href']:
+    if 'https://www.thinkslogans.com/company/' in link['href']:
         category_links.append(link)
 # print(category_links)
 
@@ -25,21 +25,13 @@ category_links = list(set(category_links))
 for id, link in enumerate(category_links):
     # Find the category and sub-category before exploring the link
     split_link = link['href'].split('/')
-    category = split_link[4]
-    sub_category_1 = ''
-    sub_category_2 = ''
-    if len(split_link) == 7:
-        sub_category_1 = split_link[5]
-    elif len(split_link) == 8:
-        sub_category_1 = split_link[5]
-        sub_category_2 = split_link[6]
-    print('\n{}: Working on category: {}, sub-category-1: {}, sub-category-2: {}'
-                .format(id + 1, category, sub_category_1, sub_category_2))
+    company = split_link[4]
+    print('\n{}: Working on company: {}.'.format(id + 1, company))
 
     # Create the possible sublinks to pages with more slogans for the category
     possible_sublinks = []
     possible_sublinks.append(link['href'])
-    for i in range(2, 101):
+    for i in range(2, 50):
         possible_sublinks.append(link['href'] + 'page/' + str(i) + '/')
 
     # Explore the link and all sublinks and if valid collect all slogans
@@ -59,16 +51,15 @@ for id, link in enumerate(category_links):
                 link_slogans.append(paragraph.get_text().strip())
 
     # Append all slogans found for this category to the global list
-    SLOGANS.append({'category': category, 'sub-category-1': sub_category_1, 'sub-category-2': sub_category_2, 'slogans':link_slogans})
+    SLOGANS.append({'company': company, 'slogans':link_slogans})
 
 # Write all slogans with category and subcategory into CSV file by creating a flattened dict list
 print('\nWriting slogans to slogans.csv')
-with open('slogans.csv', mode='w', encoding='utf-8-sig', newline='') as file:
-    fieldnames = ['category', 'sub-category-1', 'sub-category-2', 'slogan']
+with open('company-slogans.csv', mode='w', encoding='utf-8-sig', newline='') as file:
+    fieldnames = ['company', 'slogan']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
 
     writer.writeheader()
     for item in SLOGANS:
         for slogan in item['slogans']:
-            writer.writerow({'category': item['category'], 'sub-category-1': item['sub-category-1'], 
-                             'sub-category-2': item['sub-category-2'], 'slogan': slogan})
+            writer.writerow({'company': item['company'],'slogan': slogan})
